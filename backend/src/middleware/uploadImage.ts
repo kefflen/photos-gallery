bcb3588585fd5948870c458630021f93b05336de
kefflen <55823300+kefflen@users.noreply.github.com>
2021-11-06 19:41:26 -0300

    Função para renomear filename caso tenha com mesmo filename

--- backend/src/middleware/uploadImage.ts
+++ backend/src/middleware/uploadImage.ts
@@ -1,18 +1,39 @@
 import { Request } from 'express'
 import multer from 'multer'
+import fs from 'fs'
 
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, './uploads')
   },
+
   filename: function(req, file, cb) {
     let { originalname } = file
-    let createdAt = Date.now()
-    let randomNumber = Math.round(Math.random() * 10000)
-    cb(null, `${createdAt}-${randomNumber}-${originalname}`)
+    let filename = `${Date.now()} - ${originalname}`
+    console.log({filename})
+    let resolvedFilename = resolveFileName(filename)
+    if (!resolvedFilename) return cb(new Error("File has a invalid format"), 'teste')
+    return cb(null, resolvedFilename)
   }
 })
+
+function resolveFileName(nameToResolve: string) {
+  let finalName
+  let tempName = nameToResolve
+  let exist = 0
+  if (nameToResolve.split(".").length != 2) return null
+  while (!finalName || exist > 100) {
+    if (fs.existsSync(tempName)) {
+      exist++
+      let [filename, extension ] = nameToResolve.split('.')
+      tempName = filename + ` (${exist}).` + extension
+    } else {
+      finalName = tempName
+    }
+  }
+  return finalName
+}
 
 function fileFilter (req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback) {
   if (['image/jpeg', 'image/jpg', 'image/png'].includes(file.mimetype)) {
