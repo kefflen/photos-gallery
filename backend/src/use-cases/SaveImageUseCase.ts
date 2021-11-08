de9ca771e45c20035baff629e90d4bc320b24440
kefflen <55823300+kefflen@users.noreply.github.com>
2021-11-08 19:25:48 -0300

    Colocando uma verificação em SaveImageUseCase

--- backend/src/use-cases/SaveImageUseCase.ts
+++ backend/src/use-cases/SaveImageUseCase.ts
@@ -1,8 +1,12 @@
 import { db } from "../database/db";
+import { AppError } from "../helpers/Errors";
 
 export class SaveImageUseCase {
   async execute(name: string, path: string) {
-    await db('image').insert({name, path
+    const hasImageWithPath = await db('image').where({ path })
+    if (hasImageWithPath) throw new AppError("Can´t register that path", 500) //supostamente nunca vai dar true,a menos que  coloque manualmente no banco de dado
+    await db('image').insert({
+      name, path
     })
   }
 }
